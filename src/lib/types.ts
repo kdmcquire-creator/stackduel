2d7741e5b5f997c0cd9d838b98e55d789be40519
shane-mitchell-doanything <shane.mitchell1@doanything.com>
2026-03-09 22:30:35 -0700

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,17 @@
 export interface Product {
   id: string;
   name: string;
-  category: string;
-  description: string;
-  price: number;
-  rating: number;
-  reviews: number;
-  features: string[];
-  image: string;
   slug: string;
+  description: string;
+  pricing: {
+    startingPrice: string;
+    billingPeriod: string;
+    free: boolean;
+  };
+  features: string[];
+  pros: string[];
+  cons: string[];
+  rating?: number;
+  reviews?: number;
+  image?: string;
 }
